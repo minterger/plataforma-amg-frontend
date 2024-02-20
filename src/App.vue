811725408b575b23316ee4e1d2cdf0f0e0a3337a
Leonel Menci <minterger@gmail.com>
2024-02-20 02:08:31 -0300

    add

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup></script>
 
 <template>
-  <div
-    class="w-screen max-w-full h-screen max-h-screen overflow-hidden grid grid-cols-10 grid-rows-12"
-  >
-    <div class="col-span-2 row-span-12 bg-red-500"></div>
-    <div class="col-span-8 bg-blue-500"></div>
-    <div class="col-span-8 row-span-11 bg-green-500"></div>
+  <div class="w-screen max-w-full h-screen max-h-screen flex">
+    <div class="bg-red-500 h-screen w-80"></div>
+    <div class="w-full h-screen relative">
+      <div class="bg-blue-500 w-full h-14 flex justify-between absolute">
+        <div>
+          <i class="bx bx-search text-white text-2xl"></i> <input type="text" />
+        </div>
+        <div>asdasd</div>
+      </div>
+      <div class="bg-green-500 w-full h-full p-4 pt-[4.5rem]"></div>
+    </div>
   </div>
 </template>
