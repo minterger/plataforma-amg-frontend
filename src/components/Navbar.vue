c4d3eb7255db7c88931321535153498b6e6c3864
Minterger <minterger@gmail.com>
2024-02-22 13:45:32 -0300

    feat

---
+++ src/components/Navbar.vue
@@ -0,0 +1,81 @@
+<script setup>
+import { ref } from "vue";
+
+const menuMobile = ref(false);
+</script>
+
+<template>
+  <div
+    :class="menuMobile ? 'translate-x-0' : '-translate-x-full'"
+    class="fixed z-50 lg:static bg-gray-900 h-screen w-4/5 sm:w-96 p-4 lg:translate-x-0 transition-transform ease-in-out"
+  >
+    <button
+      @click="
+        () => {
+          menuMobile = !menuMobile;
+        }
+      "
+      class="absolute lg:hidden top-20 -right-12 bg-gray-900 py-2 pr-3 pl-2 rounded-r-full flex justify-end"
+    >
+      <i
+        :class="menuMobile ? 'bx-x' : 'bx-menu'"
+        class="bx text-white text-3xl"
+      ></i>
+    </button>
+
+    <img
+      src="../assets/logoAMG.png"
+      class="w-40 h-[23.7px] mt-4 w mx-auto"
+      alt=""
+    />
+
+    <ul class="text-slate-400 font-semibold mt-8 flex flex-col gap-1">
+      <li>
+        <router-link
+          class="px-2 py-1 flex gap-3 items-center rounded-md hover:bg-gray-800 hover:text-white transition-colors"
+          to=""
+          ><i class="bx bx-home text-2xl font-light"></i>Inicio</router-link
+        >
+      </li>
+      <li>
+        <router-link
+          class="px-2 py-1 flex gap-3 items-center rounded-md hover:bg-gray-800 hover:text-white transition-colors"
+          to=""
+          ><i class="bx bxs-truck text-2xl font-light"></i>Viajes</router-link
+        >
+      </li>
+      <li>
+        <router-link
+          class="px-2 py-1 flex gap-3 items-center rounded-md hover:bg-gray-800 hover:text-white transition-colors"
+          to=""
+          ><i class="bx bx-hard-hat text-2xl font-light"></i
+          >Fleteros</router-link
+        >
+      </li>
+      <li>
+        <router-link
+          class="px-2 py-1 flex gap-3 items-center rounded-md hover:bg-gray-800 hover:text-white transition-colors"
+          to=""
+          ><i class="bx bx-transfer text-2xl font-light"></i
+          >Transportes</router-link
+        >
+      </li>
+      <li>
+        <router-link
+          class="px-2 py-1 flex gap-3 items-center rounded-md hover:bg-gray-800 hover:text-white transition-colors"
+          to=""
+          ><i class="bx bx-hard-hat text-2xl font-light"></i
+          >Transportes</router-link
+        >
+      </li>
+      <li>
+        <router-link
+          class="px-2 py-1 flex gap-3 items-center rounded-md hover:bg-gray-800 hover:text-white transition-colors"
+          to=""
+          ><i class="bx bx-hard-hat text-2xl font-light"></i
+          >Transportes</router-link
+        >
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ src/components/TopNav.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from "vue";
+
+const menuUser = ref(false);
+</script>
+
+<template>
+  <div
+    class="border-b shadow-sm w-full h-16 flex items-center gap-4 absolute px-8"
+  >
+    <div class="flex items-center gap-2 w-full">
+      <label for="search"
+        ><i class="bx bx-search text-slate-400 text-2xl"></i
+      ></label>
+      <input
+        id="search"
+        class="w-full focus-visible:outline-none p-2"
+        type="text"
+        placeholder="Buscar..."
+      />
+    </div>
+    <div class="relative">
+      <button
+        @click="
+          () => {
+            menuUser = !menuUser;
+          }
+        "
+        class="font-semibold flex items-center gap-2 whitespace-nowrap"
+      >
+        Oscar Leonel Menci<i
+          class="bx bx-chevron-down text-slate-400 text-2xl"
+        ></i>
+      </button>
+      <ul
+        :class="
+          menuUser ? 'visible opacity-100' : 'invisible opacity-0 -scale-50'
+        "
+        class="absolute right-0 rounded-md bg-white shadow-lg px-4 py-2 mt-2 w-32 flex flex-col gap-2 border transition-all"
+      >
+        <li><router-link to="/">Tus Datos</router-link></li>
+        <li><button>Salir</button></li>
+      </ul>
+    </div>
+  </div>
+</template>
