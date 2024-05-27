0043bf83d6d32675b19843f83737d38b51f711d0
Leonel Menci <minterger@gmail.com>
2024-05-26 21:07:34 -0300

    update form

--- src/views/NewViaje.vue
+++ src/views/NewViaje.vue
@@ -1,18 +1,26 @@
 <script setup>
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 
-const newViaje = reactive({
+const manifiesto = reactive({
   mic: "",
   crt: "",
   cliente: "",
+});
+
+const viaje = reactive({
   origen: "",
   destino: "",
   mercaderia: "",
+});
+
+const contratacion = reactive({
   valor_flete: undefined,
   tipo_moneda: "",
   razon_social_facturacion: "",
   condicion_pago: "",
 });
+
+const descripcion = ref("");
 </script>
 
 <template>
@@ -29,6 +37,9 @@ const newViaje = reactive({
       class="border border-dashed p-4 rounded-lg shadow-md bg-white overflow-x-auto w-full"
     >
       <form @submit.prevent="">
+        <!-- -------------------- -->
+        <!-- Datos del manifiesto -->
+        <!-- -------------------- -->
         <h2 class="text-center font-semibold text-2xl pt-4 pb-2">
           Datos de Manifiesto
         </h2>
@@ -36,22 +47,22 @@ const newViaje = reactive({
           <div class="flex flex-col">
             <label class="mb-2" for="mic">Numero de MIC</label>
             <input
-              v-model="newViaje.mic"
+              v-model="manifiesto.mic"
               class="p-2 border rounded-md"
               type="text"
               id="mic"
-              placeholder="EJ: 24AR000000L"
+              placeholder="EJ: 24AR000000L (opcional)"
             />
           </div>
 
           <div class="flex flex-col">
             <label class="mb-2" for="crt">Numero de CRT</label>
             <input
-              v-model="newViaje.crt"
+              v-model="manifiesto.crt"
               class="p-2 border rounded-md"
               type="text"
               id="crt"
-              placeholder="EJ: 0003.AR.00213"
+              placeholder="EJ: 0003.AR.00213 (opcional)"
             />
           </div>
           <div class="flex flex-col md:col-span-2">
@@ -61,7 +72,7 @@ const newViaje = reactive({
               >Cliente</label
             >
             <input
-              v-model="newViaje.cliente"
+              v-model="manifiesto.cliente"
               class="p-2 border rounded-md"
               type="text"
               id="client"
@@ -70,6 +81,9 @@ const newViaje = reactive({
           </div>
         </div>
 
+        <!-- ---------------------------------------------------------------------- -->
+        <!-- Datos de donde se realiza el viaje hasta dodne y que mercadeeria lleva -->
+        <!-- ---------------------------------------------------------------------- -->
         <h2 class="text-center font-semibold text-2xl pt-4 pb-2">
           Datos de Viaje
         </h2>
@@ -81,7 +95,7 @@ const newViaje = reactive({
               >Origen</label
             >
             <input
-              v-model="newViaje.origen"
+              v-model="viaje.origen"
               class="p-2 border rounded-md"
               type="text"
               id="origen"
@@ -96,7 +110,7 @@ const newViaje = reactive({
               >Destino</label
             >
             <input
-              v-model="newViaje.destino"
+              v-model="viaje.destino"
               class="p-2 border rounded-md"
               type="text"
               id="destino"
@@ -111,7 +125,7 @@ const newViaje = reactive({
               >Mercaderia</label
             >
             <input
-              v-model="newViaje.mercaderia"
+              v-model="viaje.mercaderia"
               class="p-2 border rounded-md"
               type="text"
               id="mercaderia"
@@ -119,10 +133,13 @@ const newViaje = reactive({
             />
           </div>
         </div>
+
+        <!-- ------------------------------------------------------------------- -->
+        <!-- Datos de la empresa a facturar y valor de flete + condicion de pago -->
+        <!-- ------------------------------------------------------------------- -->
         <h2 class="text-center font-semibold text-2xl pt-4 pb-2">
           Datos de Contratación
         </h2>
-
         <div class="grid grid-cols-1 md:grid-cols-2 gap-2 border-b pb-4">
           <div class="flex flex-col">
             <label
@@ -131,9 +148,9 @@ const newViaje = reactive({
               >Valor Flete</label
             >
             <input
-              v-model="newViaje.valor_flete"
+              v-model="contratacion.valor_flete"
               class="p-2 border rounded-md"
-              type="text"
+              type="number"
               id="value"
               placeholder="EJ: 1800"
             />
@@ -145,7 +162,7 @@ const newViaje = reactive({
               >Moneda</label
             >
             <input
-              v-model="newViaje.tipo_moneda"
+              v-model="contratacion.tipo_moneda"
               class="p-2 border rounded-md"
               type="text"
               id="money"
@@ -159,7 +176,7 @@ const newViaje = reactive({
               >Razon Social a Facturar</label
             >
             <input
-              v-model="newViaje.razon_social_facturacion"
+              v-model="contratacion.razon_social_facturacion"
               class="p-2 border rounded-md"
               type="text"
               id="razon"
@@ -173,12 +190,98 @@ const newViaje = reactive({
               >Condicion de pago</label
             >
             <input
-              v-model="newViaje.condicion_pago"
+              v-model="contratacion.condicion_pago"
               class="p-2 border rounded-md"
               type="text"
               id="pago"
               placeholder="EJ: VTO DE PAGO A 30 DIAS"
             />
+          </div>
+        </div>
+
+        <!-- --------------------------------------- -->
+        <!-- Datos de la unidad que realiza el viaje -->
+        <!-- --------------------------------------- -->
+        <h2 class="text-center font-semibold text-2xl pt-4 pb-2">
+          Empresa Contratada
+        </h2>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-2 border-b pb-4">
+          <div class="flex flex-col">
+            <label
+              class="mb-2 after:content-['*'] after:ml-0.5 after:text-red-500"
+              for="razon_contratada"
+              >Razon Social</label
+            >
+            <input
+              class="p-2 border rounded-md"
+              type="text"
+              id="razon_contratada"
+              placeholder="EJ: Juan Fredes S.A."
+            />
+          </div>
+
+          <div class="flex flex-col">
+            <label
+              class="mb-2 after:content-['*'] after:ml-0.5 after:text-red-500"
+              for="cuit_rut_contratada"
+              >CUIT/RUT</label
+            >
+            <input
+              class="p-2 border rounded-md"
+              type="text"
+              id="cuit_rut_contratada"
+              placeholder="EJ: 30-74835868-4"
+            />
+          </div>
+
+          <div class="flex flex-col">
+            <label
+              class="mb-2 after:content-['*'] after:ml-0.5 after:text-red-500"
+              for="tractor_contratada"
+              >Patente Tractor</label
+            >
+            <input
+              class="p-2 border rounded-md"
+              type="text"
+              id="tractor_contratada"
+              placeholder="EJ: AA 111 AA"
+            />
+          </div>
+
+          <div class="flex flex-col">
+            <label
+              class="mb-2 after:content-['*'] after:ml-0.5 after:text-red-500"
+              for="semirremolque_contratada"
+              >Patente Semi</label
+            >
+            <input
+              class="p-2 border rounded-md"
+              type="text"
+              id="semirremolque_contratada"
+              placeholder="EJ: AA 111 AA"
+            />
+          </div>
+        </div>
+
+        <!-- ---------------------------------- -->
+        <!-- descripcion del viaje o aclaracion -->
+        <!-- ---------------------------------- -->
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-2 border-b pb-4">
+          <div class="flex flex-col col-span-2">
+            <label
+              class="mb-2 after:content-['*'] after:ml-0.5 after:text-red-500"
+              for="descripcion"
+              >Descripcion</label
+            >
+
+            <textarea
+              v-model="descripcion"
+              name="descripcion"
+              id="descripcion"
+              rows="4"
+              placeholder="Descripcion del viaje o aclaracion"
+              class="p-2 row-span-2 border rounded-md"
+            ></textarea>
           </div>
         </div>
       </form>
