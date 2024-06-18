80028b56eb1c3c453c30ded913f927616a36f696
Leonel Menci <minterger@gmail.com>
2024-06-17 23:51:53 -0300

    feat(empresa): add getEmpresas

--- src/stores/empresa.store.js
+++ src/stores/empresa.store.js
@@ -24,19 +24,44 @@ export const empresaStore = defineStore("empresa", () => {
       const data = await res.json();
 
       notify.pushNotifications({
-        type: "success",
+        type: res.status === 200 ? "success" : "danger",
         message: data.message,
       });
     } catch (error) {
-      console.log(error);
+      console.error(error);
       notify.pushNotifications({
         type: "danger",
-        message: "Internal Server Error",
+        message: "Internal Server Error: create Empresa",
+      });
+    }
+  };
+
+  const getEmpresas = async ({ filter, search, type = "ambos" }) => {
+    try {
+      const res = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/empresas?type=${type}${
+          filter ? `&filter=${filter}` : ""
+        }${search ? `&search=${search}` : ""}`,
+        {
+          headers: {
+            authorization: user.userToken,
+          },
+        }
+      );
+      const data = await res.json();
+
+      return data;
+    } catch (error) {
+      console.error(error);
+      notify.pushNotifications({
+        type: "danger",
+        message: "Internal Server Error: get Empresas",
       });
     }
   };
 
   return {
     newEmpresa,
+    getEmpresas,
   };
 });
