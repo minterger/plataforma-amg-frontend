b84f4798807058d226d003b2023dc0552cbd7ce2
Leonel Menci <minterger@gmail.com>
2024-07-02 23:46:25 -0300

    feat: Add ClienteData and TransporteData views with delete functionality

--- src/views/ClienteData.vue
+++ src/views/ClienteData.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { empresaStore } from "../stores/empresa.store";
 import { useRoute, useRouter } from "vue-router";
 import { ref } from "vue";
+import { empresaStore } from "../stores/empresa.store";
 
 const route = useRoute();
 const router = useRouter();
@@ -10,20 +10,19 @@ const id = route.params.id;
 
 const empresa = empresaStore();
 
-const transporte = ref(null);
+const cliente = ref(null);
 
 (async () => {
-  transporte.value = await empresa.getOneEmpresa({ type: "transporte", id });
-  console.log(transporte.value);
+  cliente.value = await empresa.getOneEmpresa({ type: "cliente", id });
 })();
 
 const deleteEmpresa = async () => {
   const deleted = await empresa.deleteEmpresa({
-    type: "transporte",
-    id: transporte.value?._id,
+    type: "cliente",
+    id: cliente.value?._id,
   });
 
-  if (deleted) router.push("/transportes");
+  if (deleted) router.push("/clientes");
 };
 </script>
 
@@ -33,14 +32,8 @@ const deleteEmpresa = async () => {
       <!-- boton de añadir Unidades ya sea tractor o semi, boton para editar transporte y boton para eliminar transporte -->
       <div class="w-full flex gap-2 justify-end mb-6">
         <router-link
-          to="/new/transporte"
-          class="flex items-center bg-green-700 text-white gap-2 px-4 py-2 rounded-md"
-        >
-          <i class="bx bx-plus"></i>Añadir Unidad</router-link
-        >
-        <router-link
           class="flex items-center bg-yellow-600 text-white gap-2 px-4 py-2 rounded-md"
-          :to="'/edit/transporte/' + transporte?._id"
+          :to="'/edit/cliente/' + cliente?._id"
         >
           <i class="bx bx-edit"></i>Editar
         </router-link>
@@ -53,15 +46,11 @@ const deleteEmpresa = async () => {
       </div>
     </div>
 
-    <!-- tabla de contenidos -->
+    <!-- contenidos -->
     <section
-      class="border border-dashed p-4 rounded-lg shadow-md bg-white overflow-x-auto w-full"
+      class="border border-dashed p-4 rounded-lg shadow-md bg-white overflow-auto w-full"
     >
-      <div
-        class="w-full flex items-center justify-center text-xl font-semibold"
-      >
-        <span>{{ transporte?.empresa }} - {{ transporte?.id_tributaria }}</span>
-      </div>
+      {{ cliente?.id_tributaria }} {{ cliente?.empresa }}
     </section>
   </div>
 </template>
